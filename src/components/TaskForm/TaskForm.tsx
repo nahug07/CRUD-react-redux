bd2f69d1518e48f82637341f102d574845a33aec
ngrijalba <nahug07@gmail.com>
2024-03-04 12:53:55 -0300

    feat: taskForm

---
+++ src/components/TaskForm/TaskForm.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+function TaskForm() {
+
+    const [task, setTask] = useState({
+        title: '',
+        description: ''
+    })
+
+    const handleChange = (e: { target: { name: string; value: string } }) => {
+        setTask({
+            ...task,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(task)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input name="title" type="text" placeholder="title" onChange={handleChange} />
+            <textarea name="description" placeholder="description" onChange={handleChange}></textarea>
+            <button>Save</button>
+        </form>
+    )
+}
+
+export default TaskForm
